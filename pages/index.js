03c17c305ff982ca0afa6f9a09e44d617675dd3b
Giorgio Ventura <gventuraagramonte@gmail.com>
2023-07-29 17:30:30 -0500

    Mostrando data en el home

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,65 @@
+import { fetchItems } from "@/utils/api";
+import { lightGreen, red } from "@mui/material/colors";
+import { useEffect, useState } from "react";
+
+const { Box, Typography, Grid, Paper } = require("@mui/material");
+
 const Home = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Vamos a traer la data
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const data = await fetchItems();
+      setItems(data);
+      setIsLoading(false);
+    };
+
+    // Ejecuto la funcion dentro del mismo effect
+    fetchData();
+  }, []);
+
+  const sortedItems = [...items].sort((a, b) => {
+    if (a.statusItem === b.statusItem) {
+      return a.diasItem - b.diasItem;
+    }
+
+    return a.statusItem ? -1 : 1;
+  });
+
   return (
-    <div>
-      <h1>Monitoreo de Certificados</h1>
-    </div>
+    <Box>
+      <Typography variant="h4" gutterBottom>
+        Monitoreo de Certificados by Giorgio
+      </Typography>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <Grid container spacing={2}>
+          {sortedItems.map((item) => (
+            <Grid item xs={6} sm={4} md={3} key={item._id}>
+              <Paper elevation={3}>
+                <Box
+                  p={2}
+                  sx={{
+                    backgroundColor: item.statusItem
+                      ? lightGreen["A400"]
+                      : red[500],
+                  }}
+                >
+                  <Typography variant="subtitle1">{item.nombreItem}</Typography>
+                  <Typography variant="subtitle1">
+                    Status: {item.statusItem ? "Activo" : "Inactivo"}
+                  </Typography>
+                </Box>
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Box>
   );
 };
 
