11ea81d7b9d2ad38eaab124d0993c2c85a86c76b
Giorgio Ventura <gventuraagramonte@gmail.com>
2023-07-30 18:06:38 -0500

    mantener la sesion en el navegador

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,57 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Cookies from "js-cookie";
+import jwt from "jsonwebtoken";
 import { Container, CssBaseline } from "@mui/material";
 import "@/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 import { Navbar } from "@/components/Navbar";
+import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    checkTokenValidity();
+    const tokenCheckInterval = setInterval(checkTokenValidity, 10 * 60 * 1000);
+    return () => {
+      // Clear the timer when the app is unmounted
+      clearInterval(tokenCheckInterval);
+    };
+  }, []);
+
+  const checkTokenValidity = () => {
+    const token = Cookies.get("token");
+    const fullName = Cookies.get("fullName");
+
+    if (token && fullName) {
+      try {
+        jwt.decode(token);
+        console.log("Validando el token");
+      } catch (error) {
+        console.error("Invalid token: ", error);
+        // If token is invalid o expired
+        Cookies.remove("token");
+        Cookies.remove("fullName");
+        router.push("/login");
+      }
+    } else {
+      console.log("Entrando al ese del if app");
+      router.push("/login");
+    }
+  };
+
   return (
     <>
       <CssBaseline />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        pauseOnFocusLoss
+      />
       <Navbar />
       <Container maxWidth="lg">
         <Component {...pageProps} />
--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_BASE_AUTH = "https://monitoreoniubiz.com/auth";
 const API_BASE_URL = "https://monitoreoniubiz.com/items";
 
+// Items
 export const fetchItems = async () => {
   try {
     const response = await axios.get(API_BASE_URL);
@@ -20,4 +22,10 @@ export const getItemById = async (id) => {
     console.log("Error fetching data", error);
     return [];
   }
+};
+
+// Usuarios
+export const login = async (user) => {
+  const response = await axios.post(`${API_BASE_AUTH}/login`, user);
+  return response.data;
 };
